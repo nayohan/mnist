cd13199a9a152010836865b3c3ff7edfb0fd0ce4
nayohan <nayohan13@gmail.com>
2018-04-05 15:00:07 +0000

    2층신경망

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""
 #URL의 파일다운
 import urllib.request
 mnist_url = "https://raw.githubusercontent.com/WegraLee/deep-learning-from-scratch/master/dataset/mnist.py"
@@ -5,28 +6,35 @@ a=urllib.request.urlopen(mnist_url)
 k=open("mnist.py","wb")
 k.write(a.read())
 k.close()
+"""
+import matplotlib
 
 #시간 확인 
 import time   
 start_time = time.time() 
 tmp_time = start_time
 
-#이미지 파일 불러오기
+#1.이미지 파일 불러오기
 from mnist import load_mnist
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False,  one_hot_label=True) #(60000,784)(60000,10)(10000,784)(10000,10)
-train_loss_list = []  
 
-#클래스 불러오기
+
+#2.클래스 불러오기
 from LayerNet import TwoLayerNet
 network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)   #클래스객체생성
 
-#하이퍼 파라미터
-iters_num = 2                   #반복횟수
+#3.하이퍼 파라미터 설정
+iters_num = 1                   #반복횟수
 train_size = x_train.shape[0]   #훈련데이터의 양 60000
 batch_size = 10                 #미니배치 크기 100
 learning_rate = 0.1             #학습률
 
-#학습 시작
+#경과기록
+train_loss_list = []  
+train_acc_list = []
+test_acc_list = []
+
+#4.학습 시작
 import numpy as np
 for i in range(iters_num):
     batch_mask = np.random.choice(train_size, batch_size) #(100,)
@@ -42,8 +50,16 @@ for i in range(iters_num):
         
     #학습 경과 기록
     loss = network.loss(x_batch, t_batch)
+    train_acc = network.accuracy(x_train, t_train)
+    test_acc = network.accuracy(x_test, t_test)
+    
     train_loss_list.append(loss) #리스트에 요소 추가
+    train_acc_list.append(train_acc)
+    test_acc_list.append(test_acc)
+    
     print(str(i+1) + " loss : "+str(train_loss_list[i]/batch_size))
+    print(str(i+1) +"train acc : " + str(train_acc))
+    print(str(i+1) +"test acc : " + str(test_acc))
     
     #1회 학습에 걸린시간
     mid_time = time.time()
@@ -51,6 +67,10 @@ for i in range(iters_num):
     print(str(i+1) + " time : " + str(int(one)) + "초")
     tmp_time = mid_time
     
-#총 걸린시간    
+    
+    
+    
+    
+#5.총 걸린시간    
 end_time = time.time()
 print("Running time : " + str(int(end_time-start_time)) + "초")    
