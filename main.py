4166a91d72a0db0336e1baa8d3172acdd3060f2d
nayohan <nayohan13@gmail.com>
2018-04-05 14:53:28 +0000

    2층신경망

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""
 #URL의 파일다운
 import urllib.request
 mnist_url = "https://raw.githubusercontent.com/WegraLee/deep-learning-from-scratch/master/dataset/mnist.py"
@@ -5,28 +6,29 @@ a=urllib.request.urlopen(mnist_url)
 k=open("mnist.py","wb")
 k.write(a.read())
 k.close()
+"""
 
 #시간 확인 
 import time   
 start_time = time.time() 
 tmp_time = start_time
 
-#이미지 파일 불러오기
+#1.이미지 파일 불러오기
 from mnist import load_mnist
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False,  one_hot_label=True) #(60000,784)(60000,10)(10000,784)(10000,10)
 train_loss_list = []  
 
-#클래스 불러오기
+#2.클래스 불러오기
 from LayerNet import TwoLayerNet
 network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)   #클래스객체생성
 
-#하이퍼 파라미터
+#3.하이퍼 파라미터 설정
 iters_num = 2                   #반복횟수
 train_size = x_train.shape[0]   #훈련데이터의 양 60000
 batch_size = 10                 #미니배치 크기 100
 learning_rate = 0.1             #학습률
 
-#학습 시작
+#4.학습 시작
 import numpy as np
 for i in range(iters_num):
     batch_mask = np.random.choice(train_size, batch_size) #(100,)
@@ -51,6 +53,6 @@ for i in range(iters_num):
     print(str(i+1) + " time : " + str(int(one)) + "초")
     tmp_time = mid_time
     
-#총 걸린시간    
+#5. 총 걸린시간    
 end_time = time.time()
 print("Running time : " + str(int(end_time-start_time)) + "초")    
