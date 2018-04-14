c219417245e5b66ee27cdf8974582d041ad3573e
nayohan <nayohan13@gmail.com>
2018-04-14 15:00:04 +0000

    Xavier,He초기값 추가, main.py 각각 *.py로 분리

---
+++ 03_optimizer/LayerNet.py
@@ -0,0 +1,81 @@
+from functions import *
+from collections import OrderedDict
+from backpropagation import *
+import numpy as np
+import pickle
+
+class TwoLayerNet:
+  	#초기 파라미터값 설정
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):     
+        self.params = {}
+        if 1:
+            #가중치 초기값 생성시 작동할부분
+            """
+            #가우시안 표준 정규 분포
+            self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)  
+            self.params['b1'] = np.zeros(hidden_size)                                     
+            self.params['W2'] = weight_init_std * np.random.randn(hidden_size,output_size)
+            self.params['b2'] = np.zeros(output_size)
+                        
+            #Xavier initialization
+            self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size) / np.sqrt(input_size) 
+            self.params['b1'] = np.zeros(hidden_size)                                     
+            self.params['W2'] = weight_init_std * np.random.randn(hidden_size,output_size) / np.sqrt(input_size) 
+            self.params['b2'] = np.zeros(output_size)
+            """
+            #He initialization
+            self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size) / np.sqrt(input_size/2) 
+            self.params['b1'] = np.zeros(hidden_size)                                     
+            self.params['W2'] = weight_init_std * np.random.randn(hidden_size,output_size) / np.sqrt(input_size/2) 
+            self.params['b2'] = np.zeros(output_size)
+            
+
+            #가중치 값 저장하기
+            f = open("/projects/mnist_cnn/03_optimizer/weight.pkl", 'wb')
+            pickle.dump(self.params, f)
+            f.close()
+           
+  
+        #가중치 값 불러오기기
+        f = open("/projects/mnist_cnn/03_optimizer/weight.pkl", 'rb')
+        self.params = pickle.load(f)
+        f.close()
+
+        #계층생성
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
+        self.lastLayer = SoftmaxWithLoss()
+
+    def predict(self, x):       #(100,784)                                           
+        for layer in self.layers.values():
+            x = layer.forward(x)
+        return x                #(100,10)
+    
+    def loss(self,x,t):         #(100,784)(100,10)
+        y = self.predict(x)
+        return self.lastLayer.forward(y,t)
+        
+    def accuracy(self,x,t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        t = np.argmax(t, axis=1)
+        accuracy = np.sum(y == t) / float(x.shape[0])
+        return accuracy
+       
+    def gradient(self,x,t):     #(100,784)(100,10)
+        self.loss(x,t)          #순전파
+        dout = 1                #역전파
+        dout = self.lastLayer.backward(dout)
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+                
+        grads = {}
+        grads['W1'] = self.layers['Affine1'].dw #(784,50)
+        grads['b1'] = self.layers['Affine1'].db #(50,)
+        grads['W2'] = self.layers['Affine2'].dw #(50,10)
+        grads['b2'] = self.layers['Affine2'].db #(10,)     
+        return grads
--- 03_optimizer/main.py
+++ 03_optimizer/main.py
@@ -5,203 +5,9 @@ import pickle
 from collections import OrderedDict
 import os
 from optimizer import *
+from LayerNet import TwoLayerNet
 
-#---------------------------------------------------------------------------------
-#functions.py
-#시그모이드 함수
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-"""   
-def softmax(a):
-    c = np.max(a)
-    exp_a = np.exp(a-c)
-    sum_exp_a = np.sum(exp_a)
-    y = exp_a / sum_exp_a
-    return y
-"""    
-def cross_entropy_error(y, t):
-    delta = 1e-8
-    return -np.sum( t * np.log(y + delta))
-  
-#소프트맥스 함수
-def softmax(x):
-    if x.ndim == 2:
-        x = x.T
-        x = x - np.max(x, axis=0)
-        y = np.exp(x) / np.sum(np.exp(x), axis=0)
-        return y.T
-    x = x - np.max(x) # 오버플로 대책
-    return np.exp(x) / np.sum(np.exp(x))
-"""
-def cross_entropy_error(y, t):
-    if y.ndim == 1:
-        t = t.reshape(1, t.size)
-        y = y.reshape(1, y.size)
-        
-    # 훈련 데이터가 원-핫 벡터라면 정답 레이블의 인덱스로 반환
-    if t.size == y.size:
-        t = t.argmax(axis=1)
-             
-    batch_size = y.shape[0]
-    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
-"""    
-#미분
-def numerical_gradient(f, x):
-    h = 1e-4 #0.0001
-    grad = np.zeros_like(x)
-    
-    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
-    while not it.finished:
-        idx = it.multi_index
-        tmp_val = x[idx]
-        x[idx] = float(tmp_val) + h
-        fxh1 = f(x) # f(x+h)
-        
-        x[idx] = tmp_val - h 
-        fxh2 = f(x) # f(x-h)
-        grad[idx] = (fxh1 - fxh2) / (2*h)
-        
-        x[idx] = tmp_val # 값 복원
-        it.iternext()   
-    return grad
-
-  
-#---------------------------------------------------------------------------------
-#backpropagation.py
-class Relu:
-    def __init_(self):
-        self.mask = None
-    
-    def forward(self, x):
-        self.mask = (x <= 0)
-        out = x.copy()
-        out[self.mask] = 0  
-        return out
-        
-    def backward(self, dout):
-        dout[self.mask] = 0
-        dx = dout
-        return dx
-      
-class Sigmoid:
-    def __init__(self):
-        self.out = None
-    
-    def forward(self, x):
-        out = 1 / (1 + np.exp(-x))
-        self.out = out
-        return out
-    
-    def backward(self, dout):
-        dx = dout * (1.0 - self.out) * self.out
-        return dx
-        
-class Affine:
-    def __init__(self, W, b):
-        self.W = W
-        self.b = b
-        self.x = None
-        self.dW = None
-        self.db = None
-    
-    def forward(self, x):
-        self.x = x
-        out = np.dot(x,self.W) + self.b
-        return out
-        
-    def backward(self, dout):     
-        dx = np.dot(dout, self.W.T)
-
-        self.dw = np.dot(self.x.T, dout)
-        self.db = np.sum(dout, axis=0)
-        return dx
-    
-class SoftmaxWithLoss:
-    def __init__(self):
-        self.loss = None
-        self.y = None
-        self.t = None
-        
-    def forward(self, x, t):
-        self.t = t
-        self.y = softmax(x)
-        self.loss = cross_entropy_error(self.y, self.t)
-        return self.loss
-        
-    def backward(self, dout=1):
-        batch_size = self.t.shape[0]
-        dx = (self.y - self.t) / batch_size
-        return dx
-       
-        
-#---------------------------------------------------------------------------------
-#LayerNet.py
-class TwoLayerNet:
-  	#초기 파라미터값 설정
-    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):     
-        self.params = {}
-        if 1:
-            #가중치 초기값 생성시 작동할부분
-            self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size) 
-            self.params['b1'] = np.zeros(hidden_size)                                     
-            self.params['W2'] = weight_init_std * np.random.randn(hidden_size,output_size)
-            self.params['b2'] = np.zeros(output_size)
-
-            #가중치 값 저장하기
-            f = open("/projects/mnist_cnn/03_optimizer/weight.pkl", 'wb')
-            pickle.dump(self.params, f)
-            f.close()
-           
-  
-        #가중치 값 불러오기기
-        f = open("/projects/mnist_cnn/03_optimizer/weight.pkl", 'rb')
-        self.params = pickle.load(f)
-        f.close()
-
-        #계층생성
-        self.layers = OrderedDict()
-        self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
-        self.layers['Relu1'] = Relu()
-        self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
-        self.lastLayer = SoftmaxWithLoss()
-
-    def predict(self, x):       #(100,784)                                           
-        for layer in self.layers.values():
-            x = layer.forward(x)
-        return x                #(100,10)
-    
-    def loss(self,x,t):         #(100,784)(100,10)
-        y = self.predict(x)
-        return self.lastLayer.forward(y,t)
-        
-    def accuracy(self,x,t):
-        y = self.predict(x)
-        y = np.argmax(y, axis=1)
-        t = np.argmax(t, axis=1)
-        accuracy = np.sum(y == t) / float(x.shape[0])
-        return accuracy
-       
-    def gradient(self,x,t):     #(100,784)(100,10)
-        self.loss(x,t)          #순전파
-        dout = 1                #역전파
-        dout = self.lastLayer.backward(dout)
-        layers = list(self.layers.values())
-        layers.reverse()
-        for layer in layers:
-            dout = layer.backward(dout)
-                
-        grads = {}
-        grads['W1'] = self.layers['Affine1'].dw #(784,50)
-        grads['b1'] = self.layers['Affine1'].db #(50,)
-        grads['W2'] = self.layers['Affine2'].dw #(50,10)
-        grads['b2'] = self.layers['Affine2'].db #(10,)     
-        return grads
-      
-      
-#---------------------------------------------------------------------------------
-#main.py
 #초기 mnist.pyURL의 파일다운
-
 mnist_url = "https://raw.githubusercontent.com/WegraLee/deep-learning-from-scratch/master/dataset/mnist.py"
 a=urllib.request.urlopen(mnist_url)
 os.chdir('/projects/mnist_cnn/03_optimizer')
@@ -245,7 +51,6 @@ for key in optimizers.keys():
     train_loss_list[key] = []  #딕셔너리안 리스트 생성성
     train_acc_list[key] = []
     test_acc_list[key] = []
-
 
 #4.학습 시작
 for i in range(iters_num):
@@ -292,7 +97,7 @@ for i in range(iters_num):
         print("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
         tmp_time = mid_time    
         """    
-        
+
 for key in optimizers.keys():
     print(key)
     print(" loss      : " + str(train_loss_list[key]))
